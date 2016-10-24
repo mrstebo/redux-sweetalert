36a13cc61ee6368eefac782ebaea81d608e7fd2e
Steven Atkinson <steven.atkinson@gmx.com>
2016-10-24 13:53:09 +0100

    Added an emitter.

---
+++ src/emitter.js
@@ -0,0 +1,11 @@
+import EventEmitter from 'eventemitter3';
+
+const emitter = new EventEmitter();
+
+export const EE = emitter;
+
+export default {
+  add: (component, options) => emitter.emit('add/modal', {component, options}),
+  remove: id => emitter.emit('remove/modal', id),
+  clear: () => emitter.emit('clear/all')
+};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-export * from './actions';
+export * from './actions'
+export {default as modal} from './emitter';
 export {default as reducer} from './reducer';
